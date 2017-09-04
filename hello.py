d3b5b0f60fd190e5bf26194dac2888b160a6fd60
Murray Coueslant <murray.coueslant1@marist.edu>
2017-09-04 11:00:14 -0400

    standardised quote mark use to single quotes throughout

--- hello.py
+++ hello.py
@@ -2,12 +2,12 @@
 # Author: Murray Coueslant
 # Date: 2017/09/01
 def main():
- print("Hello, instructor!")
+ print('Hello, instructor!')
  print('Good-bye!')
 main()
 
 def greet(name):
     instructor_name=name
-    print('Hello',name+"!")
-    print('Good-bye',name+'!')
+    print('Hello', name+'!')
+    print('Good-bye', name+'!')
 greet(input('Enter the name of the person you want to greet: '))
