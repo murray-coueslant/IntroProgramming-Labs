971479363c8d497e2c7b40178f8a800707a0ced4
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-09-22 11:20:27 -0400

    usability improvements. resolves #5 and #6.

--- guessing-game.py
+++ guessing-game.py
@@ -3,14 +3,25 @@
 
 animal = 'lion'
 
+
+def askUser(animal):
+    pref = input('Do you like ' + animal + 's?(Y or N):')
+    if pref.lower() == 'y':
+        print("I'm glad you like", animal + 's.')
+        quit()
+    else:
+        print("That's a shame,", animal + 's', 'are great creatures.')
+
+
 while 1:
     print('I am thinking of an animal.')
     guess = input('What do you think it is?: ')
-    if guess.lower() == animal.lower():
-        print('Well done! You guessed right.')
-        quit()
-    elif guess.lower() == 'quit':
+    if guess[0].lower() == 'q':
         print('Thanks for playing!')
+        quit()
+    elif guess.lower() == animal.lower():
+        print('Well done! You guessed right.')
+        askUser(animal)
         quit()
     else:
         print("Sorry, that isn't right, try again!")
