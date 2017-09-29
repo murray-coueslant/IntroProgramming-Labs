4a5efdbbde86eb41deba49c6d5318629fa80a5d3
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-09-29 13:01:43 -0400

    modularised the program, each section now has its own function

--- credentials.py
+++ credentials.py
@@ -5,19 +5,34 @@
 
 
 def main():
+    uname = userCreate()
+    passwd = passCreate()
+    print("Account configured. Your new email address is", uname + "@marist.edu")
 
+
+def getNames():
     # get user's first and last names
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
-    # TODO modify this to generate a Marist-style username
-    uname = first + '.' + last
+    nameList = [first, last]
+    return nameList
+
+
+def userCreate():
+    names = getNames()
+    # create marist style username
+    uname = names[0] + '.' + names[1]
+    return uname
+
+
+def passCreate():
     # ask user to create a new password
     passwd = input("Create a new password: ")
-    # TODO modify this to ensure the password has at least 8 characters
+    # do not allow the user to create a password shorter than 8 chars
     while len(passwd) < 8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
     print("The force is strong in this one…")
-    print("Account configured. Your new email address is", uname + "@marist.edu")
+    return passwd
 
 main()
