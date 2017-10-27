0eacf2290ca68b85266814444a557176b946e793
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-10-27 11:29:19 -0400

    completed the main steps of the lab, the game works as expected and i have handled out of bounds errors. now working on the checkvictory functions.

---
+++ tictactoe.py
@@ -0,0 +1,91 @@
+# CMPT 120 Intro to Programming
+# Lab #6 – Lists and Error Handling
+# Author: Your Name Here
+# Created: YYYY-MM-DD
+
+symbol = [" ", "x", "o"]
+
+
+def printRow(row):
+    # initialize output to the left border
+    # for each square in the row...
+    # add to output the symbol for this square followed by a border
+    # print the completed output for this row
+    output = '|'
+    for val in row:
+        if val == 0:
+            print_val = symbol[val]
+        elif val == 1:
+            print_val = symbol[val]
+        else:
+            print_val = symbol[val]
+        output += str(print_val) + '|'
+    print(output)
+
+
+def printSeparator():
+    print("+-+-+-+")
+
+
+def printBoard(board):
+    # print the top border
+    # for each row in the board...
+    # print the row
+    # print the next border
+    printSeparator()
+    for row in board:
+        printRow(row)
+        printSeparator()
+
+
+def markBoard(board, row, col, player):
+    # check to see whether the desired square is blank
+    # if so, set it to the player number
+    if board[row][col] == 0:
+        board[row][col] = player
+        return True
+    else:
+        print("Square already marked by player", board[row][col], "choose another square.")
+        row, col = getPlayerMove()
+        markBoard(board, row, col, player)
+
+
+def getPlayerMove():
+    row = 10
+    col = 10
+    while row > 2 or col > 2:
+        # prompt the user separately for the row and column numbers
+        row = int(input("Enter the row of the square you would like to mark: "))
+        col = int(input("Enter the column of the square you would like to mark: "))
+    return row, col  # then return that row and column instead of (0,0)
+
+
+def hasBlanks(board):
+    # for each row in the board...
+    # for each square in the row...
+    # check whether the square is blank
+    # if so, return True
+    for row in board:
+        for val in row:
+            if val == 0:
+                return True  # if no square is blank, return False
+    return False
+
+def checkVictory():
+    checkVictoryH()
+    checkVictoryV()
+    checkVictoryD()
+
+def main():
+    board = [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0]]
+    player = 1
+    while hasBlanks(board):
+        checkVictory()
+        row, col = getPlayerMove()
+        markBoard(board, row, col, player)
+        player = player % 2 + 1  # switch player for next turn
+        printBoard(board)
+
+main()
