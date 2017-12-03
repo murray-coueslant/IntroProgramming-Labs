601dcaee5944de618a6687b0b9ddbf3f0936ca05
Murray Coueslant <murray.coueslant+github@gmail.com>
2017-12-03 17:28:03 -0500

    initial gui window created

--- RobotStore_OO.py
+++ RobotStore_OO.py
@@ -25,34 +25,90 @@ products = [Product("Ultrasonic range finder", 2.50, 4), Product("Servo motor", 
             Product("Servo controller", 44.95, 5), Product("Microcontroller Board", 34.95, 7),
             Product("Laser range finder", 149.99, 2), Product("Lithium polymer battery", 8.99, 8)]
 
-def printStock():
-    print()
-    print("Available Products")
-    print("------------------")
+def printStock(output):
+    textOut = ""
+    textOut += "\n"
+    textOut += "Available Products\n"
+    textOut += "------------------\n"
     for i in range(0,len(products)):
         if products[i].quantity > 0:
-            print(str(i)+")", products[i].name , "$", products[i].price)
-            print()
+            textOut += str(i)+")" + " " + str(products[i].name) + " " + "$" + str(products[i].price) + " " + str(products[i].quantity)
+            textOut += "\n"
+    output.setText(textOut)
+
+def showIntro(app):
+    global entry, prompt, output
+    # simple greeting
+    output = Text(Point(2.5, 7), "Welcome to the Robot Shop!")
+    output.draw(app)
+    # instructions
+    prompt = Text(Point(2, 3), "How much money do you have? $")
+    prompt.draw(app)
+    # text entry box
+    entry = Entry(Point(4, 3), 6) # box holds six characters
+    entry.setText("0.00")
+    entry.draw(app)
+    # wait for user to input something... how do I know when to stop waiting?
+    while(app.getKey() != 'Return'): pass # so long as the user has not typed return, do nothing
+    return float(entry.getText())
 
 def main():
     app = GraphWin("Robot Shop", 400, 100)
     app.setCoords(0, 0, 5, 10)
-    cash = float(input("How much money do you have? $"))
+
+    cash = showIntro(app)
+    
+    app.close()
+
+    # create a new graphical window
+    app = GraphWin("Robot Shop", 480, 400)
+    app.setCoords(0, 0, 10, 10)
+
+    # remove old prompt and draw a new one
+    prompt.undraw()
+    prompt.setText("Enter the product ID and Quantity you wish to purchase: ")
+    prompt.draw(app)
+    prompt.move(2.25, -1)
+    # remove old entry box and draw a new one
+    entry.undraw()
+    entry.setText("ID QTY")
+    entry.draw(app)
+    entry.move(4.5, -1)
+
+    Rectangle(Point(4, 0.5), Point(6, 1.5)).draw(app)
+    button = Text(Point(5, 1), "Purchase")
+    button.draw(app)
+
+    output.undraw()
+    output.draw(app)
+    output.move(2.5, 0)
+
+    message = Text(Point(5, 4), "")
+    message.draw(app)
+    message1 = Text(Point(5, 5), "")
+    message1.draw(app)
     while cash > 0:
-        printStock()
-        vals = input("Enter product ID and quantity you wish to buy: ").split(" ")
+        printStock(output)
+        # save click coords in a variable for later
+        clickPoint = app.getMouse()
+
+        vals = entry.getText().split(" ")
+        entry.setText("")
         if vals[0] == "quit": 
             break
         prodId = int(vals[0])
         count = int(vals[1])
         if products[prodId].quantity >= count:
             if cash >= products[prodId].price:
-                products[prodId].modifyStock(count)
+                # products[prodId].modifyStock(count)
                 cash -= products[prodId].price * count
-                print("You purchased", count, products[prodId].name+"s.")
-                print("You have $", "{0:.2f}".format(cash), "remaining.")
+                message.setText("You purchased " + str(count) + " " +  products[prodId].name+"s." + "\n")
+                message1.setText("You have $" + "{0:.2f}".format(cash) + " remaining.")
+                message.setTextColor('blue')
+                message1.setTextColor('blue')
             else:
-                print("Sorry, you cannot afford that product.")
+                message.setText("Sorry, you cannot afford that product.")
+                message.setTextColor('red')
         else:
-            print("Sorry, we are sold out of", products[prodId].name)
+            message.setText("Sorry, we are sold out of " + products[prodId].name)
 main()
---
+++ personality_graphical.py
@@ -0,0 +1,78 @@
+import random
+from graphics import *
+                   # emotion  reward      punish     threaten joke
+personalityTable = [["anger", "surprise", "sadness", "anger", "anger"],
+                    ["disgust", "anger", "anger", "anger", "anger"],
+                    ["fear", "disgust", "disgust", "sadness", "disgust"],
+                    ["happiness", "happiness", "sadness", "fear", "happiness"],
+                    ["sadness", "happiness", "fear", "anger", "happiness"],
+                    ["surprise", "happiness", "anger", "disgust", "happiness"]]
+
+# get the interaction from the user and return an integer corresponding to the column in the table above
+#     params: none
+#     input: user interaction
+#     returns: corresponding integer
+def getInteraction():
+    interaction = input('Enter the interaction you would like to have with the personality: ').lower()
+    if interaction == 'reward':
+        return 1
+    elif interaction == 'punish':
+        return 2
+    elif interaction == 'threaten':
+        return 3
+    elif interaction == 'joke':
+        return 4
+    else:
+        return 0
+
+
+# search the table for the current emotion and return the emotion following the user interaction
+#     params: current emotion, user interaction
+#     input: none
+#     returns: new emotion
+def lookupEmotion(currentEmotion, userInteraction):
+    for row in personalityTable:
+        if row[0] == currentEmotion:
+            return row[userInteraction]
+
+
+# display the current emotion of the personality using print
+#     params: current emotion
+#     input: none
+#     returns: none
+#     output: message corresponding to current emotion
+def displayEmotion(currentEmotion):
+    if currentEmotion == 'anger':
+        print('You\'re making me very angry!')
+    elif currentEmotion == 'disgust':
+        print('You\'re a horrid person!')
+    elif currentEmotion == 'fear':
+        print('You\'re scaring me!')
+    elif currentEmotion == 'happiness':
+        print('I\'ve got a big smile on my face!')
+    elif currentEmotion == 'sadness':
+        print('You should wipe the tears from my eyes.')
+    else:
+        print('What a surprise!')
+
+
+print("Welcome to the personality, enjoy interacting with it through four commands. Threaten, punish, joke or reward.")
+newEmotion = personalityTable[random.randint(0, 5)][0]
+currentEmotion = newEmotion
+app = GraphWin("Personality", 400, 200)
+app.setCoords(0, 0, 10, 10)
+
+Text(Point(5,7), "Welcome to the interactive personality!").draw(app)
+Text(Point(4,3), "I am currently feeling " + currentEmotion).draw(app)
+Text(Point(4,1), "What would you like to do?: ").draw(app)
+
+inputBox = Entry(Point(8,1), 10)
+inputBox.draw(app)
+while 1:
+    displayEmotion(currentEmotion)
+    userInteraction = getInteraction()
+    while userInteraction == 0:
+        print("Incorrect interaction, enter another.")
+        userInteraction = getInteraction()
+    newEmotion = lookupEmotion(currentEmotion, userInteraction)
+    currentEmotion = newEmotion
